636c4c95db2b6258646bf15009ba39bc53d68b9d
ShahriarSuvin <shahariertanvir@gmail.com>
2023-10-23 17:24:01 +0600

    New js file added

---
+++ script.js
@@ -0,0 +1,12 @@
+let student = {
+    id: 123,
+    name: "Suvin",
+    age: 25,
+    height: 5.11,
+    weight: 86,
+    address: {
+        houseNo: "45, lalbag , Dhaka, 1211.",
+        roadNo: 121,
+        postCode: 1211
+    },
+};
